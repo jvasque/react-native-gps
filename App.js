bb14e67f584c81744597b48ab31d032cf4220c55
jvasque <75439239+jvasque@users.noreply.github.com>
2021-05-07 11:47:57 -0500

    Updated firebaseconfig

--- App.js
+++ App.js
@@ -8,12 +8,7 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBg15wobs-aRNI7U841KJVyTqaplHt8eag",
-  authDomain: "agroplace-2bbbc.firebaseapp.com",
-  projectId: "agroplace-2bbbc",
-  storageBucket: "agroplace-2bbbc.appspot.com",
-  messagingSenderId: "27807532405",
-  appId: "1:27807532405:web:6df3dee402bdcb4a5cff3c",
+//Here your config from console firebase
 };
 
 firebase.initializeApp(firebaseConfig);
